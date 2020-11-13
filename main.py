e6527226d2baf3823f90c27d50e236968576cb75
berardr <berardr@udel.edu>
2020-11-13 18:19:23 -0500

    start of main

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 
 
 
+import add_tag
+import mqtt_sub
+import pos_plotter
+
+if __name__ == "__main__":
+	print("Welcome to ARKARDS \nType \"add\" to add a tag to the database OR type \"plot\" to view a plot of active tags and anchors\nType \"exit\" to exit\n")
+	
+	while(1):
+		mode = input("Enter mode: ")
+		if mode == "add":
+			print("add")
+		elif mode == "plot":
+			print("plot")
+		elif mode == "exit":
+			print("exit")
+		else:
+			print("invalid mode - try again")
+
