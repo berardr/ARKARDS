eb1ba8fa1c8ca8600c4d56e4dd82b012990b038e
vrazpopov <vrazpopov@gmail.com>
2020-11-17 21:44:08 -0500

    implemented add_node, this file can add or remove nodes from the json.

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ import sys
 
 if __name__ == "__main__":
 
-	print("Welcome to ARKARDS \nType \"add\" to add a node to the database OR type \"plot\" to view a plot of active tags and anchors\nType \"exit\" to exit")
-	
+	print("Welcome to ARKARDS \nType \"edit\" to edit the database OR type \"plot\" to view a plot of active tags and anchors\nType \"exit\" to exit")
+
 	while(1):
 		try:
 			nodeinfo_q = queue.Queue()
 			startflag_q = queue.Queue()
 			mode = input("\nEnter mode: ")
-			if mode == "add":
+			if mode == "edit":
 
 				mode = add_node.start()
 			elif mode == "plot":
 
 				plot_t = threading.Thread(target=pos_plotter.start, args=(nodeinfo_q,))
-				sub_t = threading.Thread(target=mqtt_sub.start, args=(nodeinfo_q,))  
+				sub_t = threading.Thread(target=mqtt_sub.start, args=(nodeinfo_q,))
 
 				plot_t.start()
 				sub_t.start()
 				plot_t.join()
 				sub_t.join()
-			
+
 
 			elif mode == "exit":
 				print("EXITING")
